37055af70c942e05a4d2e2119268903cbccae563
wlk0202 <54826094+wlk0202@users.noreply.github.com>
2019-10-01 11:05:35 +0900

    Add files via upload

---
+++ ImgDlg.h
@@ -0,0 +1,36 @@
+﻿
+// ImgDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CImgDlg 대화 상자
+class CImgDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CImgDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+	CImage img;
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_IMG_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton1();
+};
